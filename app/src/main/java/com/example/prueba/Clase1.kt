73c315673b3fee14c71b98653f5e41647d5ad1f5
mals24 <marceloliendo24@gmail.com>
2024-11-15 00:10:55 -0500

    Se agrego IdUser a Clase1

--- app/src/main/java/com/example/prueba/Clase1.kt
+++ app/src/main/java/com/example/prueba/Clase1.kt
@@ -3,13 +3,15 @@ package com.example.prueba
 class Clase1 {
     var User:String=""
     var Pass:String=""
+    var IdUser:String=""
 
-    constructor(User: String, Pass: String) {
+    constructor(){}
+
+    constructor(User: String, Pass: String, IdUser: String) {
         this.User = User
         this.Pass = Pass
+        this.IdUser = IdUser
     }
 
-    constructor(){
 
-    }
 }
